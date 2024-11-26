5862d8adf224dc35a4121bf652929540a6078696
Mbastidas <man.bastidas@duocuc.cl>
2024-11-26 00:14:12 -0300

    Proyecto con EntityFramework y Sqlite

---
+++ AlertiSafe-Back/AlertiSafe/Program.cs
@@ -0,0 +1,39 @@
+using AlertiSafe.Components;
+using AlertiSafe.Modelo;
+using AlertiSafe.Services;
+using Microsoft.AspNetCore.Components.Authorization;
+using MudBlazor.Services;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+//Personales
+builder.Services.AddMudServices();
+builder.Services.AddScoped<IdentifierContext>();
+builder.Services.AddScoped<AuthService>();
+
+//
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
---
+++ AlertiSafe-Back/AlertiSafe/Services/AuthService.cs
@@ -0,0 +1,23 @@
+﻿using AlertiSafe.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace AlertiSafe.Services;
+
+public class AuthService
+{
+    private readonly IdentifierContext _context;
+
+    public AuthService(IdentifierContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<bool> ValidateCredentials(string username, string password)
+    {
+        // Busca un usuario con el nombre y contraseña especificados
+        var user = await _context.Usuarios
+            .FirstOrDefaultAsync(u => u.Email == username && u.contrasenia == password);
+
+        return user != null; // Retorna true si el usuario existe
+    }
+}
